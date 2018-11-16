af19616b524f73fde518e14dabd868c0567c3682
JosephSnedeker <135777@student.asd20.org>
2018-11-16 14:48:35 -0700

    started working on character maker

---
+++ JSnedeker_CharacterLoadSave/JSnedeker_CharacterLoadSave/Character.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSnedeker_CharacterLoadSave
+{
+    class Character
+    {
+        public string name;
+        public int strength;
+        public int tenacity;
+        public int utility;
+        public int perception;
+        public int intelligence;
+        public int dexterity;
+
+        public Character(string name, int strength, int tenacity, int utility, int perception, int intelligence, int dexterity)
+        {
+            this.name = name;
+            this.strength = strength;
+            this.tenacity = tenacity;
+            this.utility = utility;
+            this.perception = perception;
+            this.intelligence = intelligence;
+            this.dexterity = dexterity;
+        }
+    }
+    
+}
---
+++ JSnedeker_CharacterLoadSave/JSnedeker_CharacterLoadSave/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JSnedeker_CharacterLoadSave
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int selection;
+            List<Character> characters = new List<Character>();
+
+            ////////////////////////////////////
+            Console.WriteLine("*********************");
+            Console.WriteLine("1. Create Character");
+            Console.WriteLine("2. Modify Characater");
+            Console.WriteLine("3. Delete Characater");
+            Console.WriteLine("4. Load Characater");
+            ////////////////////////////////////
+            do
+            {
+                selection = int.Parse(Console.ReadLine()); {
+                    if (selection == 1)
+                    {
+                        characters.Add(CreateCharacter());
+                    }
+
+                    if (selection == 2)
+                    {
+                        Console.WriteLine("what character");
+                        string search = Console.ReadLine();
+                        for (int i = 0; i < characters.Count; i++)
+                        {
+                            if (characters[i].name == search)
+                                Character character = characters[i]
+
+                        }
+                    }
+    
+
+                
+
+                } while (selection != 0) ;
+            }
+            static Character CreateCharacter()
+            {
+
+            }
+            static void ModifyCharacter(Character character)
+            {
+
+            }
+            static void DeleteCharacter(List<Character> characters, string characterName)
+            {
+
+            }
+            static void LoadCharacter(string characterName)
+            {
+
+            }
+        }
+    }
+}
