4eb16ca24cfea612f135a4ed33cd942abce02791
JosephSnedeker <135777@student.asd20.org>
2019-01-09 14:50:16 -0700

    made stack class

---
+++ Semester2/Stack/Stack/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int choice = -1;
+            Stack myStack = new Stack();
+            while (choice != 0)
+            {
+                Console.WriteLine("Choose an option");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Peek");
+                Console.WriteLine("4. Print");
+                Console.WriteLine("5. Search");
+                choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.WriteLine("What value to push");
+                    myStack.Push(int.Parse(Console.ReadLine()));
+                }
+                if (choice == 2)
+                {
+                    Console.WriteLine(myStack.Pop() + " has been popped");
+                }
+                if (choice == 3)
+                {
+                    Console.WriteLine(myStack.Peek() + " has been peeked");
+                }
+                if (choice == 4)
+                {
+                    Console.WriteLine();
+
+
+                    myStack.Print();
+                }
+                if(choice == 5)
+                {
+                    List<int> foundValues;
+                    Console.WriteLine("What values are you looking for");
+                    foundValues = myStack.Search(int.Parse(Console.ReadLine()));
+                    Console.WriteLine("The value you searched for is located at ");
+                    for (int i = 0; i < foundValues.Count; i++)
+                    {
+                        Console.WriteLine(foundValues[i]);
+                    }
+                    
+                    
+                }
+                Console.ReadKey();
+                Console.Clear();
+                
+            }
+            
+        }
+    }
+}
---
+++ Semester2/Stack/Stack/stack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack
+    {
+        List<int> holder = new List<int>();
+        public Stack()
+        {
+
+        }
+        //Push
+        public void Push(int val)
+        {
+            holder.Add(val);
+        }
+        //Pop
+        public int Pop()
+        {
+            int x = holder[holder.Count - 1];
+            holder.RemoveAt(holder.Count - 1);
+            return x;  
+            
+        }
+        //Peek
+        public int Peek()
+        {
+            return holder[holder.Count - 1];
+        }
+        //Print
+        public void Print()
+        {
+            for (int i = holder.Count - 1; i > -1; i--)
+            {
+                Console.WriteLine(holder[i]);
+            }
+        }
+        public List<int> Search(int val)
+        {
+            List<int> positions = new List<int>();
+            for (int i = holder.Count - 1; i >= 0; i--)
+            {
+                if (val == holder[i])
+                {
+                    positions.Add((holder.Count - 1) -i);
+                }
+            }
+            return positions;
+        }
+    }
+
+
+}
