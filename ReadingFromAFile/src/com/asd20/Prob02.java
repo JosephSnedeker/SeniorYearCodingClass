5e116d9d4b5ac674b9f65da6e0ee98825bec12f3
JosephSnedeker <135777@student.asd20.org>
2019-04-25 14:50:06 -0600

    worked o code quest

---
+++ ReadingFromAFile/src/com/asd20/Prob02.java
@@ -0,0 +1,10 @@
+package com.asd20;
+
+public class Prob02 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- ReadingFromAFile/src/com/asd20/ReadingFromAFile.java
+++ ReadingFromAFile/src/com/asd20/ReadingFromAFile.java
@@ -1,10 +1,35 @@
 package com.asd20;
+import java.util.*;
+import java.io.*;
 
 public class ReadingFromAFile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int nTests = scanner.nextInt();
+		
+		List<Integer> names = new ArrayList<Integer>();
+		for (int i = 0; i < nTests; i++) {
+			names.add(Integer.parseInt(scanner.next())); 
+		}
+		for (int i = 0; i< nTests; i++){
+			if(names.get(i) % 3 == 0) {
+				System.out.print("code ");
+				
+			}
+			if(names.get(i) % 7 == 0) {
+				System.out.print("quest");	
+			}
+			if(names.get(i) % 7 != 0 && names.get(i) % 3 != 0) {
+				System.out.print(names.get(i));
+			}
+			System.out.println();
+		
+			
+			
+		}
+			
+			
 	}
 
 }
